eb83bc7a4ea4125d0dbdeb9400ed239c5896fb50
DeniseFafette <denisefafette@gmail.com>
2020-01-20 09:09:35 -0800

    finished boilerplate

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+// We only need the development part ... you delete below
+// Add useNullAsDefault: true,
+// filename is the name of your database
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      // location and name of the database
+      filename: './data/name.db3'
+    },
+    migrations: {
+      // location of migrations for the database
+      directory: './data/migrations'
+    },
+    seeds: {
+      // location of seed data for the database
+      directory: './data/seeds'
+    }
+  }
+};
